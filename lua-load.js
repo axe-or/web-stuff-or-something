06d270af0c28b36e0fbdb875e82983cdf01b25af
axe-or <marcosfehlauer@gmail.com>
2024-08-06 13:34:55 -0300

    Add files via upload

--- lua-load.js
+++ lua-load.js
@@ -2,18 +2,29 @@
 const luaSourceArea = document.querySelector('#lua-source');
 const runButton = document.querySelector('#lua-run');
 const clearButton = document.querySelector('#lua-clear');
+const autorunCheckbox = document.querySelector('#lua-drop-autorun');
+
+let autoRun = autorunCheckbox.checked;
 
 runButton.addEventListener('click', () => {
+	runLuaSource();
+});
+
+autorunCheckbox.addEventListener('change', (ev) => {
+	autoRun = autorunCheckbox.checked;
+});
+
+function clearLuaSource(){
+	luaSourceArea.value = '';
+	fengari.load('main = nil')();
+}
+
+function runLuaSource(){
 	const source =
 		`main = nil
 		${luaSourceArea.value}
 		main()`;
 	fengari.load(source)();
-})
-
-function clearLuaSource(){
-	luaSourceArea.value = '';
-	fengari.load('main = nil')();
 }
 
 luaSourceArea.addEventListener('drop', (ev) => {
@@ -21,10 +32,14 @@ luaSourceArea.addEventListener('drop', (ev) => {
 	const text = ev.dataTransfer.getData('text/plain');
 	clearLuaSource();
 	luaSourceArea.value = text;
+	if(autoRun){
+		console.clear();
+		runLuaSource();
+	}
 });
 
 clearButton.addEventListener('click', () => {
 	console.log(luaSourceArea.value)
 	clearLuaSource();
-})
+});
 
