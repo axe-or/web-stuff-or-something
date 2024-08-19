131a6ecce354de329fea0394e16b7d9a6793701e
axe-or <marcosfehlauer@gmail.com>
2024-08-19 14:10:35 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,16 +1,3 @@
-local function list_string(l)
-	if #l == 0 then return '[]' end
-	local s = '['
-	for _, v in ipairs(l) do
-		if type(v) == 'table' then
-			s = s .. list_string(v) .. ' '
-		else
-			s = s .. tostring(v) .. ' '
-		end
-	end
-	return s:sub(1, #s-1) .. ']'
-end
-
 local TokenKind = enum {
 	Paren_Open = '(',
 	Paren_Close = ')',
@@ -216,7 +203,7 @@ local ESC_SEQ = {
 }
 
 local function escape(c)
-	local e = ESC_SEQ
+	local e = ESC_SEQ[c]
 	if not e then
 		errof('Unknown escape sequence: \\%s', c)
 	end
